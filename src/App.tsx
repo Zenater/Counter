10bca963a51722448e7c3d6ed0e771db4e4aabe8
Zenater <zent230096@mail.ru>
2022-03-06 17:51:40 +0300

    changed components

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,8 @@ function App() {
     let [count, setCount] = useState(0);
     let [startValue, setStartValue] = useState(0)
     let [maxCount, setMaxCount] = useState(0)
-
     let [disButton, setDisButton] = useState<boolean>(false)
     let [error, setError] = useState('')
-
 
     const increment = () => {
         count++
@@ -22,7 +20,11 @@ function App() {
             setCount(startValue)
         }
     };
-
+    const forSet = () => {
+        if (startValue > 0) {
+            setCount(startValue)
+        }
+    }
     const onChangeStart = (e: ChangeEvent<HTMLInputElement>) => {
         let start = +e.currentTarget.value;
         setStartValue(start)
@@ -32,23 +34,26 @@ function App() {
         setMaxCount(max)
     }
 
-    const forSet = () => {
-        if (startValue > 0) {
-            setCount(startValue)
-        }
-    }
-
-    if (startValue < 0 || maxCount < 0) {
+    if (startValue < 0 || maxCount < 0 ||count === maxCount ||
+        startValue===maxCount || startValue === maxCount ||
+        startValue > maxCount) {
         disButton = (true)
     }
-
+    // const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`
+    // const finalInputClassName = `${s.errorInput} ${error ? s.errorInput: s.superInput} ${className}`
     return (
         <div className="App">
+<div>
+    <Count error={error} setError={setError} count={count}
+           startValue={startValue}
+           setCount={setCount}
+           maxCount={maxCount}
+    />
 
-            <Count error={error} setError={setError} count={count}
-                   startValue={startValue} setCount={setCount} forSet={forSet}
-            />
-            <button disabled={count === maxCount} onClick={increment}>inc</button>
+</div>
+
+            <button disabled={disButton}
+                    onClick={increment}>inc</button>
             <button onClick={reset}>reset</button>
             <span>start value</span>
             <input type="number" value={startValue} onChange={onChangeStart}/>
--- src/Count.tsx
+++ src/Count.tsx
@@ -1,5 +1,5 @@
 import React, {Dispatch, SetStateAction} from 'react';
-
+import s from './Counter.module.css'
 
 type countPropsType = {
     setError: Dispatch<SetStateAction<string>>
@@ -7,8 +7,8 @@ type countPropsType = {
     count:number
     startValue: number
     setCount: (startValue:number)=>void
-    forSet:()=>void
-
+    // forSet:()=>void
+    maxCount: number
 }
 
 
@@ -16,18 +16,23 @@ export const Count= (props: countPropsType) => {
 
 
 
-    if (props.startValue < 0) {
-        props.setError('incorect')
-    } else if (props.startValue > 0) {
-        props.setError('enter values and press set')
-
+    if (props.startValue < 0 ||
+        props.startValue=== props.maxCount ||
+        props.startValue > props.maxCount) {
+        props.setError('incorect value')
+        props.setCount(+'')
     }
-
-
+    else if (props.startValue > 0 || props.maxCount>0 ) {
+       props.setError('enter values and press set')
+    }
 
     return (
    <>
-       {props.error ? props.error:props.startValue}
+       {props.count || <div className={props.error ? s.red : '' }>{props.error} </div>}
+       {/*{props.error ? props.error: props.startValue}*/}
    </>
     );
 };
+// const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`
+
+// {error && <div className={error ? s.someClass: ''}>warning</div>}
